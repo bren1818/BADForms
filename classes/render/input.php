3d667d6592821ffb4ac1fc3c7e6e665bb161ecbf
brendon irwin <bren1818@gmail.com>
2015-02-02 22:21:03 -0500

    update before bed

--- classes/render/input.php
+++ classes/render/input.php
@@ -17,7 +17,9 @@
 						id="input_<?php echo $this->formObject->getFormID().'_'.$this->formObject->getId(); ?>" 
 						type="text" placeholder="<?php echo $this->formObject->getPlaceHolder(); ?>" 
 						value="<?php echo $this->formObject->getDefaultVal(); ?>"
+						<?php if($this->formObject->getRegex() != "" ){ ?>
 						pattern="<?php echo $this->formObject->getRegex(); ?>"
+						<?php } ?>
 						data-min-length="<?php echo $this->formObject->getMinLength(); ?>" 
 						data-max-length="<?php echo $this->formObject->getMaxLength(); ?>"
 						maxlength="<?php echo $this->formObject->getMaxLength(); ?>"
